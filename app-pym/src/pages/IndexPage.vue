7f9d2344588cb1802d514fbd4a6b819ded102b8f
pedrozam <pedro.zambrana.rivera@gmail.com>
2026-03-06 16:08:24 -0400

    Tailwind integrado, proyecto quasar base

--- app-pym/src/pages/IndexPage.vue
+++ app-pym/src/pages/IndexPage.vue
@@ -1,5 +1,12 @@
 <template>
   <q-page class="row items-center justify-evenly">
+    <div class="bg-amber-200 p-2.5">
+      Hola esto es tailwind
+      <svg width="28" height="28">
+        <use xlink:href="/src/assets/svg/sprite.svg#x"/>
+      </svg>
+
+    </div>
     <example-component
       title="Example component"
       active
@@ -17,27 +24,27 @@ import ExampleComponent from 'components/ExampleComponent.vue';
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1',
+    content: 'ct1'
   },
   {
     id: 2,
-    content: 'ct2',
+    content: 'ct2'
   },
   {
     id: 3,
-    content: 'ct3',
+    content: 'ct3'
   },
   {
     id: 4,
-    content: 'ct4',
+    content: 'ct4'
   },
   {
     id: 5,
-    content: 'ct5',
-  },
+    content: 'ct5'
+  }
 ]);
 
 const meta = ref<Meta>({
-  totalCount: 1200,
+  totalCount: 1200
 });
 </script>
