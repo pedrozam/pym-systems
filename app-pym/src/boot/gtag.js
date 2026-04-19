a0f669f84cb2a89e3112da4a46ed44261f37cea6
pedrozam <pedro.zambrana.rivera@gmail.com>
2026-04-19 01:56:22 -0400

    ajustes GA4 para que funcione analitycs en quasar

---
+++ app-pym/src/boot/gtag.js
@@ -0,0 +1,12 @@
+import { boot } from 'quasar/wrappers'
+import VueGtag from 'vue-gtag'
+
+export default boot(({ app, router }) => {
+  app.use(VueGtag, {
+    config: {
+      id: 'G-ECXZDJ8G58' // 👈 Reemplaza con tu ID real
+    },
+    appName: 'PymSystems',
+    pageTrackerScreenviewEnabled: true // Trackea automáticamente las páginas
+  }, router)
+})
